e43fb75577e1044d31b8ac15e8c78d265b2849ed
rejepov01 <rejepov01@gmail.com>
2020-05-20 16:19:38 -0400

    Spring2020B18

--- src/OfficeHours/Practice_05_20_20/House.java
+++ src/OfficeHours/Practice_05_20_20/House.java
@@ -9,17 +9,30 @@ public class House {
 
         Dog dog2 = new Dog("Tuzik",6,"brown","small","regular");
 
+
         System.out.println( dog1 );
         System.out.println( dog2 );
+
+        dog2.bark();
+      //  dog2.scratch();
+
+        System.out.println("=====================================");
 
         Cat cat1 = new Cat("Donut",4,"brownie","medium","catwille");
       //  cat1.setInfo("Donut",4,"brownie","medium","catwille");
 
         System.out.println( cat1 );
 
+        cat1.scratch();
+      //  cat1.bark();
+
+        System.out.println("===============================");
+
         Bunny bunny1 = new Bunny("Bug",3,"white","small","tiny");
 
         System.out.println( bunny1 );
+        bunny1.dig();
+      //  bunny1.scratch();
 
     }
 }
