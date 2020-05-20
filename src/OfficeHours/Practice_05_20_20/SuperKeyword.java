c940eb805f5d5a76990a3d68dc78969e615b9911
rejepov01 <rejepov01@gmail.com>
2020-05-20 17:02:33 -0400

    Spring2020B18

---
+++ src/OfficeHours/Practice_05_20_20/SuperKeyword.java
@@ -0,0 +1,30 @@
+package OfficeHours.Practice_05_20_20;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+class Data{
+    int a = 100;
+
+    public void method(){
+        System.out.println("Hello World");
+    }
+}
+
+public class SuperKeyword extends Data {
+    int a = 200;
+
+    public void method() {
+        System.out.println("hello Cybertek");
+    }
+
+    public SuperKeyword() {
+        method();
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        SuperKeyword obj = new SuperKeyword();
+
+    }
+
+}
