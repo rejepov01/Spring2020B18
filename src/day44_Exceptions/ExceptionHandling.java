3e5c532a2cb37627ceb775b45b7852ec9d4679e0
rejepov01 <rejepov01@gmail.com>
2020-05-28 12:54:21 -0400

    Spring2020B18

---
+++ src/day44_Exceptions/ExceptionHandling.java
@@ -0,0 +1,47 @@
+package day44_Exceptions;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        String str = "Cybertek";
+
+        try {
+            System.out.println( str.charAt(100));
+        }catch (RuntimeException e){
+           String description = e.getMessage();
+            System.out.println(description);
+
+        }
+
+        System.out.println("=======================================================");
+
+        String result = "";
+
+        try {
+            System.out.println( 100 / 0 );
+            System.out.println("Try block");
+
+        }catch (ArithmeticException e){
+            System.out.println("Catch Block");
+           result = e.getMessage();
+
+        }
+
+        System.out.println( result);
+
+        System.out.println("Completed");
+    }
+}
+
+/*
+testCase
+     step1
+     step2
+     step3
+     step4
+     ...
+     step10
+
+
+ */
